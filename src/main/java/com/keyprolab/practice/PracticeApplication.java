b5e1af7ca1d8486dca68d04de9ad20f952777df3
keyproco <oussama.keddar@epitech.eu>
2023-01-31 07:34:15 +0100

    Create a get mappping to /, it returns restaurant details

--- src/main/java/com/keyprolab/practice/PracticeApplication.java
+++ src/main/java/com/keyprolab/practice/PracticeApplication.java
@@ -2,10 +2,9 @@ package com.keyprolab.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 
 @SpringBootApplication
@@ -16,8 +15,41 @@ public class PracticeApplication {
 		SpringApplication.run(PracticeApplication.class, args);
 	}
 
+
+
 	@GetMapping("/")
-	public String cars() {
-		return "It Works";
+	Restaurant restaurant() {
+		return new Restaurant(1L, "Pizzio", "Den ROUDBA", "pizza");
+	}
+
+	class Restaurant {
+		public Long getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getOwner() {
+			return owner;
+		}
+
+		public String getSpeciality() {
+			return speciality;
+		}
+
+		private final Long id;
+		private final String name;
+		private final String owner;
+		private final String speciality;
+
+		Restaurant(Long id, String name, String owner, String speciality) {
+			this.id = id;
+			this.name = name;
+			this.owner = owner;
+			this.speciality = speciality;
+		}
+
 	}
 }
