119a8f9da09586d524f6dd17f5e497ce382aa776
keyproco <oussama.keddar@epitech.eu>
2023-02-04 23:19:09 +0100

    add Post mehtod to create a restaurant, the logic wip

--- src/main/java/com/keyprolab/practice/restaurant/RestaurantController.java
+++ src/main/java/com/keyprolab/practice/restaurant/RestaurantController.java
@@ -2,6 +2,8 @@ package com.keyprolab.practice.restaurant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,6 +20,12 @@ public class RestaurantController {
     @GetMapping("/")
     List<Restaurant> restaurants() {
         return  restaurantService.restaurants();
+    }
+
+    @PostMapping
+    void createNewRestaurant(@RequestBody Restaurant restaurant) {
+        System.out.println("Restaurant created");
+        System.out.println(restaurant);
     }
 
 }
